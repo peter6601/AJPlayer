2a940b607fa49973c23148ad5fedf6c25b03372c
DinDin <din.din@bigbear.me>
2024-09-06 14:54:31 +0800

    [Add] add asset

--- AJPlayer/AJPlayerAsset.swift
+++ AJPlayer/AJPlayerAsset.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+import UIKit
+
+public enum AJPlayerAsset: String {
+    case play
+    case pause
+    case sliderThumb
+    case fullScreen
+    case endFullScreen
+    case seek
+    case playNext
+    case replay
+    case brightness
+    case volume_on
+    case volume_off
+    case brightness_on
+}
+
+open class AJPImageResource {
+    
+    static public func get(_ asset: AJPlayerAsset) -> UIImage? {
+        let frameworkBundle = Bundle(for: AJPImageResource.self)
+        let bundleURL = frameworkBundle.resourceURL?.appendingPathComponent("AJPlayer.bundle")
+        let resourceBundle = Bundle(url: bundleURL!)
+        let image = UIImage(named: asset.rawValue, in: resourceBundle, compatibleWith: nil)
+        return image?.withRenderingMode(.alwaysOriginal)
+    }
+}
