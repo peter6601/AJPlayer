8cb043037bd89422c7022d015fd9ca43c03b8e75
DinDin <din.din@bigbear.me>
2024-09-09 15:16:45 +0800

    [Add] add uility

--- AJPlayer/AJPlayerUtility.swift
+++ AJPlayer/AJPlayerUtility.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+import UIKit
+
+class MSSPlayerUtility {
+    static func formatSecondsToString(_ seconds: TimeInterval) -> String {
+        if seconds.isNaN {
+            return "00:00"
+        } else {
+            let min = Int(floor(seconds) / 60)
+            let sec = Int(floor(seconds).truncatingRemainder(dividingBy: 60))
+            return String(format: "%02d:%02d", min, sec)
+        }
+    }
+    
+    static func getTextSizeBy(_ text: String, font: UIFont, superView: UIView) -> CGSize {
+        let options: NSStringDrawingOptions = [.usesFontLeading, .usesLineFragmentOrigin]
+        let attributes = [NSAttributedString.Key.font: font]
+        let constrainedSize = CGSize(width: superView.bounds.width - 40,
+                                     height: CGFloat.greatestFiniteMagnitude)
+        var bounds = (text as NSString).boundingRect(with: constrainedSize,
+                                                     options: options,
+                                                     attributes: attributes,
+                                                     context: nil)
+        bounds.size = CGSize(width: bounds.width, height: bounds.height + 20)
+        return bounds.size
+    }
+}
