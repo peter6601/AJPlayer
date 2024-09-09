6a6c50a2f754c6511d54f81e1f7db49d21a9e55d
DinDin <din.din@bigbear.me>
2024-09-09 15:19:27 +0800

    [Add] add timeSlider

--- AJPlayer/AJTimeSlider.swift
+++ AJPlayer/AJTimeSlider.swift
@@ -5,4 +5,30 @@
 //  Created by DinDin on 2024/8/29.
 //
 
-import Foundation
+import SwiftUI
+
+import UIKit
+
+public class AJTimeSlider: UISlider {
+    override open func trackRect(forBounds bounds: CGRect) -> CGRect {
+        let trackHeigt:CGFloat = bounds.height * 2 / 25
+        let position = CGPoint(x: 0 , y: (bounds.height - 1) / 2 - trackHeigt / 2)
+        let customBounds = CGRect(origin: position, size: CGSize(width: bounds.size.width, height: trackHeigt))
+        super.trackRect(forBounds: customBounds)
+        return customBounds
+    }
+    
+    override open func thumbRect(forBounds bounds: CGRect, trackRect rect: CGRect, value: Float) -> CGRect {
+        let rect = super.thumbRect(forBounds: bounds, trackRect: rect, value: value)
+        let radius = (bounds.width * 0.02)
+        let newx = rect.origin.x - (radius > 3 ? 3: radius)
+        let width = bounds.height / 2
+        let height = bounds.height / 2
+        let newRect = CGRect(x: newx, y: bounds.height / 4, width: width, height: height)
+        return newRect
+    }
+    
+    override open func beginTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+        return true
+    }
+}
